3b3399d81d4826ce98c47d702fca45aa7cc007c8
Shamil Siddique <43869372+shamilsdq@users.noreply.github.com>
2022-09-11 01:35:43 +0530

    Setup Home page (#2)
    * App cleanup and setup baseUrl
    
    * Created an empty Home page component
    
    * Added image in assets
    
    * Created Intro component for Home
    
    * Added Intro component to Home page and added styling
    
    * Updated styling for Intro
    
    * Added feather icons to the app
    
    * Added Github and Linkedin links to Intro
    
    * Setup styling for the social links
    
    * Updated social links to open in a new tab

---
+++ components/Home/Intro.tsx
@@ -0,0 +1,46 @@
+import { FunctionComponent } from "react";
+import Image from "next/image";
+
+import { GitHub, Linkedin } from "react-feather";
+import dpImage from "assets/images/onam-1-cropped.png";
+import styles from "styles/Home.module.css";
+
+const Content: FunctionComponent = () => (
+    <p>
+        Hi, I'm <span>Shamil Siddique</span>, a full-stack developer from
+        Kerala.
+        <br />I love good code, good food and good people.
+    </p>
+);
+
+const Social: FunctionComponent = () => (
+    <ul>
+        <li>
+            <a href="https://linkedin.com/in/shamilsdq" target="_blank">
+                <Linkedin size="1.25rem" />
+                <span>LinkedIn</span>
+            </a>
+        </li>
+        <li>
+            <a href="https://github.com/shamilsdq" target="_blank">
+                <GitHub size="1.25rem" />
+                <span>GitHub</span>
+            </a>
+        </li>
+    </ul>
+);
+
+const Intro: FunctionComponent = () => (
+    <section className={styles.introContainer}>
+        <div className={styles.introImage}>
+            <Image src={dpImage} layout="responsive" />
+        </div>
+
+        <div className={styles.introContentContainer}>
+            <Content />
+            <Social />
+        </div>
+    </section>
+);
+
+export default Intro;
