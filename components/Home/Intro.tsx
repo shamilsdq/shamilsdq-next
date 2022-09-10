b7be78d6f5b6a6523008b9cfea093211e0f79396
shamilsdq <shamilsdq@live.com>
2022-09-11 01:26:54 +0530

    Added Github and Linkedin links to Intro

--- components/Home/Intro.tsx
+++ components/Home/Intro.tsx
@@ -1,14 +1,33 @@
 import { FunctionComponent } from "react";
 import Image from "next/image";
 
+import { GitHub, Linkedin } from "react-feather";
 import dpImage from "assets/images/onam-1-cropped.png";
 import styles from "styles/Home.module.css";
 
 const Content: FunctionComponent = () => (
     <p>
-        Hi, I'm <span>Shamil Siddique</span>.
+        Hi, I'm <span>Shamil Siddique</span>, a full-stack developer from
+        Kerala.
         <br />I love good code, good food and good people.
     </p>
+);
+
+const Social: FunctionComponent = () => (
+    <ul>
+        <li>
+            <a href="https://linkedin.com/in/shamilsdq">
+                <Linkedin size="1.25rem" />
+                <span>LinkedIn</span>
+            </a>
+        </li>
+        <li>
+            <a href="https://github.com/shamilsdq">
+                <GitHub size="1.25rem" />
+                <span>GitHub</span>
+            </a>
+        </li>
+    </ul>
 );
 
 const Intro: FunctionComponent = () => (
@@ -19,6 +38,7 @@ const Intro: FunctionComponent = () => (
 
         <div className={styles.introContentContainer}>
             <Content />
+            <Social />
         </div>
     </section>
 );
