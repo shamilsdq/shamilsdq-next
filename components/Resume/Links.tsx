ae67fc0594100d81a316d203868c31af536080e7
shamilsdq <shamilsdq@live.com>
2022-09-14 14:50:46 +0530

    Moved links to constants and utilized it

--- components/Resume/Links.tsx
+++ components/Resume/Links.tsx
@@ -1,27 +1,24 @@
 import { FunctionComponent } from "react";
 import styles from "styles/Resume.module.css";
 
+type LinkType = { label: string; url: string };
+
+const linksList: Array<LinkType> = [
+    { label: "LinkedIn", url: "https://linkedin.com/in/shamilsdq" },
+    { label: "GitHub", url: "https://github.com/shamilsdq" },
+    { label: "HackerRank", url: "https://hackerrank.com/shamilsdq" },
+    { label: "LeetCode", url: "https://leetcode.com/shamilsdq" },
+];
+
 const Links: FunctionComponent = () => (
     <section className={styles.links}>
         <h4>Links</h4>
         <ul>
-            <li>
-                <a href="https://linkedin.com/in/shamilsdq">LinkedIn</a>
-            </li>
-            <li>
-                <a href="https://github.com/shamilsdq">GitHub</a>
-            </li>
-            <li>
-                <a href="https://hackerrank.com/shamilsdq">HackerRank</a>
-            </li>
-            <li>
-                <a href="https://leetcode.com/shamilsdq">LeetCode</a>
-            </li>
-            <li>
-                <a href="https://stackoverflow.com/users/4657143/shamilsdq">
-                    Stack Overflow
-                </a>
-            </li>
+            {linksList.map(({ label, url }) => (
+                <li>
+                    <a href={url}>{label}</a>
+                </li>
+            ))}
         </ul>
     </section>
 );
