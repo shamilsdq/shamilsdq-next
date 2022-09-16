5962dd30bc5e39bd03be862d80c26423d043f63c
shamilsdq <shamilsdq@live.com>
2022-09-16 10:20:44 +0530

    Added content for Courses

--- components/Resume/Courses.tsx
+++ components/Resume/Courses.tsx
@@ -3,6 +3,19 @@ import { FunctionComponent } from "react";
 const Courses: FunctionComponent = () => (
     <section>
         <h3>Courses</h3>
+        <div>
+            <article>
+                <h5>Algorithmic Toolbox, University of California San Diego</h5>
+                <span>Coursera: November 2020</span>
+            </article>
+            <article>
+                <h5>
+                    Introduction to Data Science in Python, University of
+                    Michigan
+                </h5>
+                <span>Coursera: October 2020</span>
+            </article>
+        </div>
     </section>
 );
 
