6647116c03c05de531ff161d77e2208b19047f98
shamilsdq <shamilsdq@live.com>
2022-09-16 09:59:37 +0530

    Added content for EducationHistory component

--- components/Resume/Education.tsx
+++ components/Resume/Education.tsx
@@ -3,6 +3,25 @@ import { FunctionComponent } from "react";
 const EducationHistory: FunctionComponent = () => (
     <section>
         <h3>Education History</h3>
+        <div>
+            <article>
+                <h5>
+                    Bachelor of Technology in Computer Science, Cochin
+                    University of Science and Technology
+                </h5>
+                <span>July 2017 - May 2021</span>
+                <ul>
+                    <li>
+                        Major Project: Answer Sheet Auditor (Django, Flutter
+                        Web, NLP)
+                    </li>
+                    <li>
+                        Minor Project: Medicine Availability Locator (Django,
+                        Flutter)
+                    </li>
+                </ul>
+            </article>
+        </div>
     </section>
 );
 
