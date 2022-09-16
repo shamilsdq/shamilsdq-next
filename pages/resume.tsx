d726bcb9152523c874eae99c21f81b5ea9feeaaf
shamilsdq <shamilsdq@live.com>
2022-09-16 12:59:05 +0530

    Added a styled info regarding resume and download link in resume page

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -14,23 +14,42 @@ import {
 } from "components/Resume";
 
 const Resume: FunctionComponent = () => (
-    <div className={styles.resume}>
-        <Title />
-        <div>
-            <aside>
-                <Details />
-                <Links />
-                <Skills />
-            </aside>
-            <main>
-                <Profile />
-                <EmploymentHistory />
-                <EducationHistory />
-                <Courses />
-                <Projects />
-            </main>
+    <>
+        <p className={styles.resumeInfo}>
+            This page is optimized for printing the resume. You can use the{" "}
+            <b>print option from your browser</b> to print or save this resume
+            as a PDF. Alternatively, you can{" "}
+            <b>download this resume as a PDF</b> with this{" "}
+            <a
+                href="/downloads/resume.pdf"
+                rel="noreferrer"
+                target="_blank"
+                download
+            >
+                download link
+            </a>
+            .
+        </p>
+        <div className={styles.resumeContainer}>
+            <div className={styles.resume}>
+                <Title />
+                <div>
+                    <aside>
+                        <Details />
+                        <Links />
+                        <Skills />
+                    </aside>
+                    <main>
+                        <Profile />
+                        <EmploymentHistory />
+                        <EducationHistory />
+                        <Courses />
+                        <Projects />
+                    </main>
+                </div>
+            </div>
         </div>
-    </div>
+    </>
 );
 
 export default Resume;
