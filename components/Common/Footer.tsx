56b523ea1bc35cb78ef9fbf59aa2dea321154fbe
shamilsdq <shamilsdq@live.com>
2022-10-10 10:37:25 +0530

    Added repo link in Footer

--- components/Common/Footer.tsx
+++ components/Common/Footer.tsx
@@ -3,6 +3,9 @@ import { FunctionComponent } from "react";
 const Footer: FunctionComponent = () => (
     <footer id="footer">
         <span>Shamil Siddique &copy; 2022</span>
+        <a href="https://github.com/shamilsdq/shamilsdq-next" target="_blank">
+            Github Repo
+        </a>
     </footer>
 );
 
