f9685b8f1d83e513345a99873364c75ea51f46c2
shamilsdq <shamilsdq@live.com>
2022-12-31 15:41:17 +0530

    Removed component splitting inside home intro

--- components/Home/Intro.tsx
+++ components/Home/Intro.tsx
@@ -6,35 +6,27 @@ import styles from "styles/Home.module.css";
 
 import { SOCIAL_LINKS } from "./constants";
 
-const Content: FC = () => (
-    <p>
-        Hi, I&apos;m <span>Shamil Siddique</span>, a full-stack developer from
-        Kerala.
-        <br />I love good code, good food and good people.
-    </p>
-);
-
-const Social: FC = () => (
-    <ul>
-        {SOCIAL_LINKS.map(({ label, url, icon: IconComponent }) => (
-            <li key={label}>
-                <a href={url} target="_blank" rel="noreferrer">
-                    <IconComponent size="1.25rem" />
-                    <span>{label}</span>
-                </a>
-            </li>
-        ))}
-    </ul>
-);
-
 const Intro: FC = () => (
     <section className={styles.introContainer}>
         <div className={styles.introImage}>
             <Image src={dpImage} layout="responsive" alt="Shamil Siddique" />
         </div>
         <div className={styles.introContentContainer}>
-            <Content />
-            <Social />
+            <p>
+                Hi, I&apos;m <span>Shamil Siddique</span>, a full-stack
+                developer from Kerala.
+                <br />I love good code, good food and good people.
+            </p>
+            <ul>
+                {SOCIAL_LINKS.map(({ label, url, icon: IconComponent }) => (
+                    <li key={label}>
+                        <a href={url} target="_blank" rel="noreferrer">
+                            <IconComponent size="1.25rem" />
+                            <span>{label}</span>
+                        </a>
+                    </li>
+                ))}
+            </ul>
         </div>
     </section>
 );
